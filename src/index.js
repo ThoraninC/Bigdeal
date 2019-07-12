a0d13817f574acd7eb1591140efb564b07a156d9
ThoraninC <sodakoi01@hotmail.com>
2019-07-12 18:55:10 +0700

    fixcard_andsomesource

--- src/index.js
+++ src/index.js
@@ -1,188 +1,101 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
-function Square(props){
-    return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
-    );
-}
-  
-class Board extends React.Component {
+
+class card extends React.Component{
+
     constructor(props){
-        super(props);
+        super(props)
         this.state = {
-            squares: Array(9).fill(null),
-            xIsNext:true,
-        };
+            suit: null,
+            rank: null,
+        }
     }
 
-    renderSquare(i) {
-      return (
-        <Square 
-            value={this.props.squares[i]}
-            onClick={() => this.props.onClick(i)}
-        />
-      );
-    }
-
-
-    render() {
-
-        let table = []
-
-        for(let i=0;i<3;i++){
-            let cells = []
-            for(let j=0;j<3;j++){
-                cells.push(this.renderSquare(j*3+i));
-            }
-            table.push(<div className="board-row">{cells}</div>);
+    skin_switch(card_skin){
+        switch(card_skin){
+            case "standard":
+                return "\\Playing_Cards\\PNG-cards-1.3";
+            default:
+                return "\\Playing_Cards\\PNG-cards-1.3";
         }
 
-        return (
-            // <div>
-            //     <div className="board-row">
-            //         {this.renderSquare(0)}
-            //         {this.renderSquare(1)}
-            //         {this.renderSquare(2)}
-            //     </div>
-            //     <div className="board-row">
-            //         {this.renderSquare(3)}
-            //         {this.renderSquare(4)}
-            //         {this.renderSquare(5)}
-            //     </div>
-            //     <div className="board-row">
-            //         {this.renderSquare(6)}
-            //         {this.renderSquare(7)}
-            //         {this.renderSquare(8)}
-            //     </div>
-            // </div>
-
-            <div>
-                {table}
-            </div>
-        );
-    }
-}
-  
-class Game extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            history: [{
-                squares: Array(9).fill(null),
-            }],
-            stepNumber: 0,
-            xIsNext: true,
-        };
     }
 
-    handleClick(i,row,col){
-        const history = this.state.history.slice(0, this.state.stepNumber + 1);
-        const current = history[history.length - 1];
-        const squares = current.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-            return;
+    suffix_switch(card_skin){
+        switch(card_skin){
+            case "standard":
+                return ".png";
+            default:
+                return ".png";
+        }
+    }
+
+    rank_switch(rank){
+        switch(rank){
+            case "A":
+                return "ace_of";
+            case "2":
+                return "2_of";
+            case "3":
+                return "3_of";
+            case "4":
+                return "4_of";
+            case "5":
+                return "5_of";
+            case "6":
+                return "6_of";
+            case "7":
+                return "7_of";
+            case "8":
+                return "8_of";
+            case "9":
+                return "9_of";
+            case "T":
+                return "10_of";
+            case "J":
+                return "jack2_of";
+            case "Q":
+                return "queen2_of";
+            case "K":
+                return "king2_of";
+            default:
+                return null;
+        }
+    }
+
+    suit_switch(suit){
+        switch(suit){
+            case "C":
+                return "_clubs";
+            case "D":
+                return "_diamonds";
+            case "H":
+                return "_hearts";
+            case "S":
+                return "_spades";
+            default:
+                return null;
+        }
+    }
+
+    getcard_pic(card_skin,rank,suit){
+
+        var cardpicture;
+
+       
+
+        switch(this.state.rank){
+
         }
 
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            history: history.concat([{
-              squares: squares,
-              row: row,
-              col: col,
-            }]),
-            this_move: i,
-            stepNumber: history.length,
-            xIsNext: !this.state.xIsNext
-        });
-    }
 
-    jumpTo(step){
-        this.setState({
-            stepNumber: step,
-            xIsNext: (step % 2) === 0,
-        });
-
-        
-    }
-
-    render() {
-        const history = this.state.history;
-        const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
-
-        const moves = history.map((step, move) => {
-        var desc;
-            if(move){
-                if(move === this.state.stepNumber){
-                    desc = '<Text style={{fontWeight: "bold"}}> Go to move #' + move + "(" + history[move].row + "," + history[move].col + ")</text>";
-                }else{
-                    desc = 'Go to move #' + move + "(" + history[move].row + "," + history[move].col + ")";
-                }
-            }else{
-                desc = 'Go to game start';
-            }
-
-            return (
-                <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
-                </li>
-            );
-        
-
-        });
-    
-        let status;
-        if (winner) {
-          status = 'Winner: ' + winner;
-        } else {
-          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board
-                squares={current.squares}
-                onClick={i => this.handleClick(i,parseInt(i/3),i%3)}
-            />
-          </div>
-          <div className="game-info">
-            <div>{status}</div>
-            <ol>{moves}</ol>
-          </div>
-        </div>
-      );
+        return <img src={cardpicture} alt="Logo" />;
     }
 }
 
-function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-  
 // ========================================
   
 ReactDOM.render(
-    <Game />,
     document.getElementById('root')
 );
-  
